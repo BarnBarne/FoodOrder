323bca1bc47dad6c30d478c2b1c2e5823d9a3ca1
Michal <tanot33@gmail.com>
2023-10-31 23:48:22 +0100

    sending orders to backend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
 import CartProvider from './store/CartProvider'
-import Checkout from './components/Checkout/Checkout'
+
 
 function App() {
 	const [cartIsShown, setCartIsShown] = useState(false)
@@ -20,7 +20,6 @@ function App() {
 	return (
 		<CartProvider>
 			{cartIsShown && <Cart onClose={hideCartHandler} />}
-			{cartIsShown && <Checkout />}
 			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,52 +8,64 @@ import classes from './AvailableMeals.module.css'
 
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
+	const [isLoading, setIsLoading] = useState(true)
 	const [error, setError] = useState(null)
 
 	useEffect(() => {
 		const fetchMeals = async () => {
 			setError(null)
-			try {
-				setIsLoading(true)
-				const response = await fetch(
-					'https://react-http-93277-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
-				)
-				if (!response.ok) {
-					throw new Error('Something is wrong')
-				}
-				const responseData = await response.json()
 
-				const loadedMeals = []
-
-				for (const key in responseData) {
-					loadedMeals.push({
-						id: key,
-						name: responseData[key].name,
-						description: responseData[key].description,
-						price: responseData[key].price,
-					})
-				}
-
-				setMeals(loadedMeals)
-			} catch (error) {
-				setError(error.message)
+			const response = await fetch('https://react-http-93277-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+			if (!response.ok) {
+				throw new Error('Something is wrong')
 			}
+			const responseData = await response.json()
+
+			const loadedMeals = []
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				})
+			}
+
+			setMeals(loadedMeals)
+
 			setIsLoading(false)
 		}
 
-		fetchMeals()
+		fetchMeals().catch(error => {
+			setIsLoading(false)
+			setError(error.message)
+		})
 	}, [])
 	const mealsList = meals.map(meal => (
 		<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
 	))
 
+	if (isLoading) {
+		return (
+			<section className={classes.MealsLoading}>
+				<p>Loading...</p>
+			</section>
+		)
+	}
+
+	if (error) {
+		return (
+			<section className={classes.MealsError}>
+				<p>{error}</p>
+			</section>
+		)
+	}
+
 	return (
 		<section className={classes.meals}>
 			<Card>
-				{!isLoading && <ul>{mealsList}</ul>}
-				{isLoading && <p>loading content</p>}
-				{error && <p>{error}</p>}
+				<ul>{mealsList}</ul>
 			</Card>
 		</section>
 	)
